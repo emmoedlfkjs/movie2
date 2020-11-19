53961443d767c9af79c326d241d8c6b9092d35ed
eunah jang <73974308+emmoedlfkjs@users.noreply.github.com>
2020-11-19 18:50:30 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies:[],
+    pictures:[]
+  };
+ componentDidMount() {
+   setTimeout(() =>{
+    this.setState({isLoading:false});
+ },6000);
+}
+  render(){
+   const{isLoading}=this.state;
+    return<div>{isLoading ? "Loading": "We are ready"}</div>;
+  }
+  }
+export default App;
